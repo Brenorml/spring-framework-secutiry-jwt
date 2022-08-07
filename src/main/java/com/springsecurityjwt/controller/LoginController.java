fb7aa4f131c32eb9dd1f23ed1a8ea83120800655
Brenorml <breno.lucena@gmail.com>
2022-08-07 16:48:51 +0100

    Adicionado login e controler de login, sessão e controler de validação

---
+++ src/main/java/com/springsecurityjwt/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.springsecurityjwt.controller;
+import com.springsecurityjwt.dtos.Login;
+import com.springsecurityjwt.dtos.Sessao;
+import com.springsecurityjwt.model.User;
+import com.springsecurityjwt.repository.UserRepository;
+import com.springsecurityjwt.security.JWTCreator;
+import com.springsecurityjwt.security.JWTObject;
+import com.springsecurityjwt.security.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+@RestController
+public class LoginController {
+    @Autowired
+    private PasswordEncoder encoder;
+    @Autowired
+    private SecurityConfig securityConfig;
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/login")
+    public Sessao logar(@RequestBody Login login){
+        User user = repository.findByUsername(login.getUsername());
+        if(user!=null) {
+            boolean passwordOk =  encoder.matches(login.getPassword(), user.getPassword());
+            if (!passwordOk) {
+                throw new RuntimeException("Senha inválida para o login: " + login.getUsername());
+            }
+            //Estamos enviando um objeto Sessão para retornar mais informações do usuário
+            Sessao sessao = new Sessao();
+            sessao.setLogin(user.getUsername());
+
+            JWTObject jwtObject = new JWTObject();
+            jwtObject.setIssuedAt(new Date(System.currentTimeMillis()));
+            jwtObject.setExpiration((new Date(System.currentTimeMillis() + SecurityConfig.EXPIRATION)));
+            jwtObject.setRoles(user.getRoles());
+            sessao.setToken(JWTCreator.create(SecurityConfig.PREFIX, SecurityConfig.KEY, jwtObject));
+            return sessao;
+        }else {
+            throw new RuntimeException("Erro ao tentar fazer login");
+        }
+    }
+}
